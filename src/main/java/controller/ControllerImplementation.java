70b72d7ca412bb6ae3c47666c16fe4554aef6c11
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-05-07 09:22:48 +0200

    #5 Information message added when inserted successfully

--- src/main/java/controller/ControllerImplementation.java
+++ src/main/java/controller/ControllerImplementation.java
@@ -385,6 +385,7 @@ public class ControllerImplementation implements IController, ActionListener {
         try {
             if (dao.read(p) == null) {
                 dao.insert(p);
+                JOptionPane.showMessageDialog(insert, "Person inserted succesfully!", "Insert - People v1.1.0", JOptionPane.INFORMATION_MESSAGE);
             } else {
                 throw new PersonException(p.getNif() + " is registered and can not "
                         + "be INSERTED.");
