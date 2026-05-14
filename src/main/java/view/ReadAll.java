2219b4374cd00ae55e2049d473670c32fdbdf001
Johan <johanhenryag@gmail.com>
2026-05-14 13:40:39 +0200

    Implement export people data CSV functionality #10

--- src/main/java/view/ReadAll.java
+++ src/main/java/view/ReadAll.java
@@ -25,7 +25,10 @@ public class ReadAll extends javax.swing.JDialog {
     public JTable getTable() {
         return table;
     }
-
+    
+    public javax.swing.JButton getExportData() {
+        return exportData;
+    }
     
     /**
      * This method is called from within the constructor to initialize the form.
@@ -40,6 +43,7 @@ public class ReadAll extends javax.swing.JDialog {
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
+        exportData = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Read All - People v1.1.0");
@@ -91,11 +95,14 @@ public class ReadAll extends javax.swing.JDialog {
         gridBagConstraints.insets = new java.awt.Insets(12, 24, 12, 24);
         getContentPane().add(jLabel2, gridBagConstraints);
 
+        exportData.setText("EXPORT DATA");
+        getContentPane().add(exportData, new java.awt.GridBagConstraints());
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton exportData;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable table;
